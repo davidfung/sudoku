5710eb734a439a9968559a75a4d55700f9355fea
davidfung <davidfung@amgcomputing.com>
2026-03-24 14:52:39 -0700

    Add board printing & flow graph

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import "fmt"
+import "strings"
 
-var calls int
+var iterations int
+var depth int
+var maxdepth int
 
 var board = [9][9]int{
 	{1, 0, 7, 0, 0, 6, 4, 5, 0},
@@ -16,23 +19,38 @@ var board = [9][9]int{
 	{0, 4, 0, 0, 7, 8, 5, 9, 1},
 }
 
+func printBoard() {
+	for r:=0; r<9; r++ {
+		for c:=0; c<9; c++ {
+			fmt.Printf("%d ", board[r][c])
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
 	fmt.Println("sudoku!")
-	fmt.Println(board)
-	calls = 0
+	printBoard()
+	iterations = 0
 
 	if solveSudoku() {
 		fmt.Println("Solved!")
-		fmt.Println(board)
+		printBoard()
 	} else {
 		fmt.Println("No solution exists.")
 	}
 
-	fmt.Printf("Total iterations: %d\n", calls)
+	fmt.Printf("Total iterations: %d\n", iterations)
+	fmt.Printf("Max depth: %d\n", maxdepth)
 }
 
 func solveSudoku() bool {
-	calls++
+	iterations++
+	depth++
+	fmt.Printf("%s\n", strings.Repeat("*", depth))
+	if depth > maxdepth {
+		maxdepth = depth
+	}
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
 			if board[i][j] == 0 {
@@ -40,15 +58,18 @@ func solveSudoku() bool {
 					if isValid(i, j, num) {
 						board[i][j] = num
 						if solveSudoku() {
+							depth--
 							return true
 						}
 						board[i][j] = 0
 					}
 				}
+				depth--
 				return false
 			}
 		}
 	}
+	depth--
 	return true
 }
 
